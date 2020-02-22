d0582b0c2188d4748f673a0a476a3b686b92e50a
coffeeteer <tgrieshiemer@gmail.com>
2020-02-21 21:20:43 -0500

    Project finished

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,43 @@
 <template>
-    <div class="container">
-    
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="createNew"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="col-sm-12">
+      <h4 class="alert alert-info text-center">Info: Click on quote to delete quote.</h4>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import Header from "./components/Header.vue";
+
+export default {
+  data: function() {
+    return {
+      quotes: ["This is a test Quote"],
+      maxQuotes: 10
+    };
+  },
+  methods: {
+    createNew(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Please delete quotes first!");
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
     }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  }
+};
 </script>
 
 <style>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row">
+    <form>
+      <div class="col-sm-6 col-sm-offset-6">
+        <label>Add Quote</label>
+        <textarea rows="3" class="form-control" v-model="quote"></textarea>
+      </div>
+      <div class="col-sm-6 col-sm-offset-6">
+        <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      quote: ""
+    };
+  },
+  methods: {
+    createNew() {
+      this.$emit("quoteAdded", this.quote);
+      this.quote = "";
+    }
+  }
+};
+</script>
